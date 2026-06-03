8596cbf24d4112db37516a09eee4220b358ab4ad
krushangmalaviya717 <krushangmalaviya717@gmail.com>
2026-06-03 09:22:50 +0530

    Update SMTP config

--- server.js
+++ server.js
@@ -18,10 +18,16 @@ const razorpay = new Razorpay({
 
 // Setup Nodemailer transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
